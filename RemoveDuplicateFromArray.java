7d7f48d39c32bc2f0d670754d527029e919661ef
UdayReddy1210 <60789584+UdayReddy1210@users.noreply.github.com>
2022-03-11 10:04:41 +0530

    Add files via upload

---
+++ RemoveDuplicateFromArray.java
@@ -0,0 +1,43 @@
+package javaTraining;
+
+
+	import java.util.Arrays;
+	public class RemoveDuplicateFromArray {
+
+	   public static void main(String[] args) {
+	      
+	      int arr[] = {10, 10, 20, 30, 40, 40, 50};
+	      
+	     
+	      int newArr[] = removeDuplicates(arr);
+	      
+	      
+	      System.out.println("Original array: "
+	                     + Arrays.toString(arr));
+	      System.out.println("After removing duplicates: " 
+	                     + Arrays.toString(newArr));
+	      
+	   }
+	   
+	   
+	   public static int[] removeDuplicates(int[] arr) {
+	      
+	      int j = 0; 
+	      int lastIndex = arr.length-1; 
+	      
+	      
+	      for (int i = 0; i < lastIndex; i++) {
+	         if(arr[i] != arr[i+1]) {
+	            arr[j++] = arr[i];
+	         }
+	      }
+	      
+	      
+	      if(arr[j] != arr[lastIndex])
+	         arr[j++] = arr[lastIndex];
+	      
+	      
+	      return Arrays.copyOf(arr, j);
+	   }
+	}
+
